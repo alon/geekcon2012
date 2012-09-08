d7b2b570e82b2ce3dbfe4d7b311ca1cbbd858321
Alon Levy <alevy@redhat.com>
2012-09-08 10:39:29 +0300

    support alon's machine as well as yoni's

--- detection/readframe.py
+++ detection/readframe.py
@@ -1,11 +1,25 @@
+import os
+
 import numpy
 import cv2.cv as cv
 
+locations = [
+"D:/Users/Jonathan/Desktop/opencv/samples/python2/video/recs/yoni/rgb",
+"/spaceil/geekcon2012/video/recs/yoni/rgb"
+]
+
+for location in locations:
+    if os.path.exists(location):
+        file_template = '%s/%%05d' % location
+        break
+else:
+    print "missing rgb images directory, not any of %s" % repr(locations)
+    raise SystemExit
 
 file_frame = 0
 def read_one_frame():
     global file_frame
-    filename = "D:/Users/Jonathan/Desktop/opencv/samples/python2/video/recs/yoni/rgb/%05d" % file_frame
+    filename = file_template % file_frame
     file_frame += 1
     with open(filename, 'rb') as fd:
         s = fd.read()
