2cad5a116fb270a898c416802674f98594c69125
Alon Levy <alevy@redhat.com>
2012-09-08 10:40:20 +0300

    refactor detect.py for vide

--- detection/detect.py
+++ detection/detect.py
@@ -1,3 +1,5 @@
+import time
+
 from operator import attrgetter
 import numpy as np
 from numpy import average
@@ -14,7 +16,6 @@ DEFAULT_THRESHOLD = 32
 MAX_TIME_DELTA = 0.25
 MIN_TIME_DELTA = 0.05
 
-import time
 TRACKER_OK = 1
 TRACKER_DEAD = 0
 RELEVANT_NUMOF_HITS = 5
@@ -29,8 +30,8 @@ class Tracker(object):
         centers_to_rects = dict(zip(centers,rects))
         global trackers
         #print 'received %d centers' % len(centers)
-        t=time.time()
-        cutoff_time = t-Tracker.TIME_WINDOW_SEC
+        t = time.time()
+        cutoff_time = t - Tracker.TIME_WINDOW_SEC
         kill_set = set()
         cx_cy_set = set()
         for tracker in trackers:
@@ -101,35 +102,23 @@ def draw_motion_comp(vis, (x, y, w, h), angle, color):
     cv2.circle(vis, (cx, cy), r, color, 3)
     cv2.line(vis, (cx, cy), (int(cx+np.cos(angle)*r), int(cy+np.sin(angle)*r)), color, 3)
 
-if __name__ == '__main__':
-    import sys
-    try: video_src = sys.argv[1]
-    except: video_src = 0
-
-    cv2.namedWindow('motempl')
-    visuals = ['input', 'frame_diff', 'motion_hist', 'grad_orient']
-    cv2.createTrackbar('visual', 'motempl', 2, len(visuals)-1, nothing)
-    cv2.createTrackbar('threshold', 'motempl', DEFAULT_THRESHOLD, 255, nothing)
-
-    #cam = video.create_capture(video_src, fallback='synth:class=chess:bg=../cpp/lena.jpg:noise=0.01')
-    cam = readframe.FakeCam()
-    ret, frame = cam.read()
-
-    h, w = frame.shape[:2]
-    prev_frame = frame.copy()
-    motion_history = np.zeros((h, w), np.float32)
-    hsv = np.zeros((h, w, 3), np.uint8)
-    hsv[:,:,1] = 255
-    while True:
-        ret, frame = cam.read()
-        frame_diff = cv2.absdiff(frame, prev_frame)
+class VideoTracker(object):
+    def __init__(self, initial_frame):
+        self.prev_frame = frame.copy()
+        h, w = initial_frame.shape[:2]
+        self.motion_history = np.zeros((h, w), np.float32)
+        self.hsv = hsv = np.zeros((h, w, 3), np.uint8)
+        hsv[:,:,1] = 255
+    def on_frame(self, frame):
+        h, w = frame.shape[:2]
+        frame_diff = cv2.absdiff(frame, self.prev_frame)
         gray_diff = cv2.cvtColor(frame_diff, cv2.COLOR_BGR2GRAY)
         thrs = cv2.getTrackbarPos('threshold', 'motempl')
         ret, motion_mask = cv2.threshold(gray_diff, thrs, 1, cv2.THRESH_BINARY)
         timestamp = clock()
-        cv2.updateMotionHistory(motion_mask, motion_history, timestamp, MHI_DURATION)
-        mg_mask, mg_orient = cv2.calcMotionGradient( motion_history, MAX_TIME_DELTA, MIN_TIME_DELTA, apertureSize=5 )
-        seg_mask, seg_bounds = cv2.segmentMotion(motion_history, timestamp, MAX_TIME_DELTA)
+        cv2.updateMotionHistory(motion_mask, self.motion_history, timestamp, MHI_DURATION)
+        mg_mask, mg_orient = cv2.calcMotionGradient(self.motion_history, MAX_TIME_DELTA, MIN_TIME_DELTA, apertureSize=5 )
+        seg_mask, seg_bounds = cv2.segmentMotion(self.motion_history, timestamp, MAX_TIME_DELTA)
 
         visual_name = visuals[cv2.getTrackbarPos('visual', 'motempl')]
         if visual_name == 'input':
@@ -137,12 +126,13 @@ if __name__ == '__main__':
         elif visual_name == 'frame_diff':
             vis = frame_diff.copy()
         elif visual_name == 'motion_hist':
-            vis = np.uint8(np.clip((motion_history-(timestamp-MHI_DURATION)) / MHI_DURATION, 0, 1)*255)
+            vis = np.uint8(np.clip(
+                (self.motion_history - (timestamp - MHI_DURATION)) / MHI_DURATION, 0, 1) * 255)
             vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
         elif visual_name == 'grad_orient':
-            hsv[:,:,0] = mg_orient/2
-            hsv[:,:,2] = mg_mask*255
-            vis = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            self.hsv[:,:,0] = mg_orient / 2
+            self.hsv[:,:,2] = mg_mask * 255
+            vis = cv2.cvtColor(self.hsv, cv2.COLOR_HSV2BGR)
 
         centers = []
         rects = []
@@ -151,10 +141,10 @@ if __name__ == '__main__':
             area = rw*rh
             if area < 64**2:
                 continue
-            silh_roi   = motion_mask   [y:y+rh,x:x+rw]
-            orient_roi = mg_orient     [y:y+rh,x:x+rw]
-            mask_roi   = mg_mask       [y:y+rh,x:x+rw]
-            mhi_roi    = motion_history[y:y+rh,x:x+rw]
+            silh_roi   = motion_mask        [y:y+rh,x:x+rw]
+            orient_roi = mg_orient          [y:y+rh,x:x+rw]
+            mask_roi   = mg_mask            [y:y+rh,x:x+rw]
+            mhi_roi    = self.motion_history[y:y+rh,x:x+rw]
             if cv2.norm(silh_roi, cv2.NORM_L1) < area*0.05:
                 continue
             angle = cv2.calcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION)
@@ -180,9 +170,27 @@ if __name__ == '__main__':
 
         draw_str(vis, (20, 20), visual_name)
         cv2.imshow('motempl', vis)
-        time.sleep(0.5)
+        #time.sleep(0.5)
+        self.prev_frame = frame.copy()
 
-        prev_frame = frame.copy()
+
+if __name__ == '__main__':
+    import sys
+    try: video_src = sys.argv[1]
+    except: video_src = 0
+
+    cv2.namedWindow('motempl')
+    visuals = ['input', 'frame_diff', 'motion_hist', 'grad_orient']
+    cv2.createTrackbar('visual', 'motempl', 2, len(visuals)-1, nothing)
+    cv2.createTrackbar('threshold', 'motempl', DEFAULT_THRESHOLD, 255, nothing)
+
+    #cam = video.create_capture(video_src, fallback='synth:class=chess:bg=../cpp/lena.jpg:noise=0.01')
+    cam = readframe.FakeCam()
+    ret, frame = cam.read()
+    video_tracker = VideoTracker(frame)
+    while True:
+        ret, frame = cam.read()
+        video_tracker.on_frame(frame)
         if 0xFF & cv2.waitKey(5) == 27:
             break
-    cv2.destroyAllWindows() 			
+    cv2.destroyAllWindows()
